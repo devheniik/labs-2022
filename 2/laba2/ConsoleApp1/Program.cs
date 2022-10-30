8626e8de92a2d755010b805e23e0f303ac41ce95
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2022-10-31 00:41:07 +0200

    234234

---
+++ 2/laba2/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿const int a = 1000000;
+const long b = 100000000000;
+const float c = 1.9f;
+const double d = 5.9;
+const short e = 2;
+const bool f = true;
+const char g = 'a';
+const string i = "Some Text";
+
+Console.WriteLine($"d / e = {d/e}");
+Console.WriteLine($"d * e = {d*e}");
+Console.WriteLine($"d + e = {d+e}");
+Console.WriteLine($"d % e = {d%e}");
+Console.WriteLine($"d - e = {d-e}");
