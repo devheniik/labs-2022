cee9e49e32fef8641bc19e6a593092567d8ddf16
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2022-11-02 03:01:07 +0200

    laba 3

---
+++ 2/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿const int a = 1000000;
+const long b = 100000000000;
+const float c = 1.9f;
+const double d = 5.9;
+const short e = 2;
+const bool f = true;
+const char g = 'a';
+const string i = "Some Text";
+
+Console.WriteLine($"d / e = {d/e}");
+Console.WriteLine($"d * e = {d*e}");
+Console.WriteLine($"d + e = {d+e}");
+Console.WriteLine($"d % e = {d%e}");
+Console.WriteLine($"d - e = {d-e}");
+
+Console.ReadKey();
---
+++ 3/laba3/laba3/Program.cs
@@ -0,0 +1,109 @@
+﻿// VARIANT 9
+
+Console.WriteLine("Selcet task id:");
+start:
+
+int n;
+if(!int.TryParse(Console.ReadLine(), out n)) {
+    Console.WriteLine("Input corrent task id:");
+    goto start;
+} 
+
+if (n != 1 && n != 2 && n != 3)
+{
+    Console.WriteLine("Input corrent task id:");
+    goto start;
+}
+
+if (n == 1)
+{ 
+
+    double x;
+
+    Console.WriteLine("Input x:");
+     
+    while (!double.TryParse(Console.ReadLine(), out x)){
+        Console.WriteLine("Input corrent x:");
+    } 
+     
+    var y = Math.Sqrt(
+            Math.Pow(x, 2) + (5 * x) + 8
+        ); 
+
+    Console.WriteLine($"y: {y}");
+
+
+}
+else if (n == 2)
+{
+
+    double x;
+
+    Console.WriteLine("Input side of the square:");
+
+    while (!double.TryParse(Console.ReadLine(), out x))
+    {
+        Console.WriteLine("Input corrent side of the square:");
+    }
+
+    Console.WriteLine($"P: {4 * x}, S: {x * x}");
+
+}
+else if (n == 3)
+{
+    double x;
+    double b;
+    double c;
+
+    start_3: 
+
+    Console.WriteLine("Input x:");
+
+    while (!double.TryParse(Console.ReadLine(), out x))
+    {
+        Console.WriteLine("Input corrent x:");
+    }
+
+    Console.WriteLine("Input b:");
+start_4:
+    while (!double.TryParse(Console.ReadLine(), out b))
+    {
+        
+        Console.WriteLine("Input corrent b:");
+    }
+
+    if (b >= x)
+    {
+        Console.WriteLine("x must be bigger then b, try again:");
+        goto start_4;
+    }
+
+    Console.WriteLine("Input c:");
+
+    while (!double.TryParse(Console.ReadLine(), out c))
+    {
+        Console.WriteLine("Input corrent c:");
+    }
+
+    var ch = 2 * x - c;
+    var zn = Math.Sqrt(Convert.ToDouble( x - b)); 
+    var abs = x - c;
+
+    if (abs < 0) {
+        abs = -1 * abs;
+    }
+
+    if(zn == 0)
+    {
+        Console.WriteLine("(x - b) must be not zero");
+        goto start_3;
+    }
+
+    Console.WriteLine($"y: {ch / zn - abs}");
+
+
+}
+
+
+
+    Console.ReadKey();
