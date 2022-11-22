a9e2f18b6c4692b6521f0bb5700c9615a35ed8f2
dev-eugen <75083421+dev-eugen@users.noreply.github.com>
2022-11-22 21:01:44 +0200

    4

--- 3/laba3/laba3/Program.cs
+++ 3/laba3/laba3/Program.cs
@@ -8,18 +8,19 @@ namespace Laba
         {
             return false;
         }
+
         //static double read_double(string varname, Func<bool> validate = (double x) => true)
-        static double read_double(string varname, Func<double, bool> validate) 
+        static double read_double(string varname, Func<double, bool> validate = null) 
             { 
                 
                 double variable; 
                 Console.WriteLine($"Input {varname}:"); 
                 while (!double.TryParse(Console.ReadLine(), out variable))
                 {
-                    Console.WriteLine($"Input corrent {varname}:");
+                    Console.WriteLine($"Please pnput correct value for the {varname}: ");
                 } 
 
-                if (validate(variable))
+                if (validate != null && validate(variable))
                 {
                     variable = read_double(varname, validate); 
                 }
@@ -35,7 +36,7 @@ namespace Laba
                 {
                     Console.WriteLine($"Input corrent {varname}:");
                 } 
-                if (variable != 1 && variable != 2 && variable != 3)
+                if (variable > 0 && variable < 4)
                 {
                    variable = read_task_number(varname); 
                 } 
@@ -49,7 +50,7 @@ namespace Laba
 
                 if (n == 1)
                 { 
-                    double x = read_double("x", defaultValidate); 
+                    double x = read_double("x"); 
                     var y = Math.Sqrt(Math.Pow(x, 2) + (5 * x) + 8);  
                     Console.WriteLine($"y: {y}");
 
@@ -57,36 +58,34 @@ namespace Laba
                 }
                 else if (n == 2)
                 { 
-                    double x = read_double("side of the square", defaultValidate);  
+                    double x = read_double("side of the square");  
                     Console.WriteLine($"P: {4 * x}, S: {x * x}"); 
                 }
                 else if (n == 3)
                 {
-                    double x = read_double("x", defaultValidate);
+                    double x = read_double("x");
 
-                    double b = read_double("b", (e) => e >= x);
+                    double b = read_double("b", e => {
+                        if (e >= x) {
+                            Console.WriteLine("");
+                            return true;
+                        }
+                        return false;
+                        });
 
-                    double c = read_double("c", defaultValidate); 
+                    double c = read_double("c"); 
 
                     var ch = 2 * x - c;
-                    var zn = Math.Sqrt(Convert.ToDouble( x - b)); 
+                    var zn = Math.Sqrt(x - b); 
                     var abs = x - c;
-
                     if (abs < 0) {
                         abs = -1 * abs;
                     }
-                     
-
                     Console.WriteLine($"y: {ch / zn - abs}");
-
-
-                }
-
-
-
-
+                    Console(Math.PI)
 
                 }
+            }
 
 
 /*public double input()
---
+++ 4/4/4/Program.cs
@@ -0,0 +1,82 @@
+﻿// VARIANT 9
+
+
+namespace Laba
+{
+    class Program
+    {
+        static bool defaultValidate(double x)
+        {
+            return false;
+        }
+
+        static bool restart()
+        {
+            
+
+            Console.WriteLine($"If you want to restart application, write 'yes' [no]");
+            string variable = Console.ReadLine();
+
+            if (variable == "yes") {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        static ulong read_ulong(string varname, Func<ulong, bool> unvalidate = null)
+        {
+
+            ulong variable;
+            Console.WriteLine($"Input {varname}:");
+            while (!ulong.TryParse(Console.ReadLine(), out variable))
+            {
+                Console.WriteLine($"Please input correct value for the {varname}, this variable must be more that 2 and SIMPLE number (not fractional), at last less than 18446744073709551616 so as ulong");
+            }
+
+            if (unvalidate != null && unvalidate(variable))
+            {
+                variable = read_ulong(varname, unvalidate);
+            }
+
+            return variable;
+        }
+        public static bool IsPrime(ulong number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (ulong)Math.Floor(Math.Sqrt(number));
+
+            for (ulong i = 3; i <= boundary; i += 2) 
+                if (number % i == 0) 
+                    return false; 
+                        
+
+            return true;
+        }
+
+        public static double getMersenNumber(ulong p)
+        {
+            return Math.Pow(2, p) - 1;
+        }
+
+
+
+        static void Main(string[] args)
+        { 
+
+            do { 
+                ulong n = read_ulong("n", arg => arg < 2); 
+                ulong p = 1; 
+                do { 
+                    Console.WriteLine(getMersenNumber(p)); 
+                    do {
+                        p++;  
+                    } while (!IsPrime(p)); 
+                } while (getMersenNumber(p) <= n); 
+            } while (restart());
+        }
+    }
+
+}
